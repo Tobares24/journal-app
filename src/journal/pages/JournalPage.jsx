d7d741316450a5ecc089b2cb4492a8837799bdb4
Steven Tobares <steventobares65@gmail.com>
2023-12-27 22:55:18 -0600

    Componentes sidebar y navbar finalizados

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,18 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography>JournalPage</Typography>
-    </>
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil adipisci
+        excepturi voluptatem repudiandae doloribus amet modi, autem debitis
+        aliquam facere placeat possimus, nulla asperiores totam. Libero
+        architecto illo deleniti modi. Lorem, ipsum dolor sit amet consectetur
+        adipisicing elit. Eos doloribus cumque maxime nobis vero repudiandae
+        omnis, culpa iusto expedita voluptatem qui repellat libero nihil ipsa.
+        Minus aliquam fugit praesentium odio!
+      </Typography>
+    </JournalLayout>
   );
 };
