acd31dd4358368c13ed6e6717969d398ef63994f
Steven Tobares <steventobares65@gmail.com>
2024-02-07 22:53:24 -0600

    Finalizacion de eliminado de journal en firebase

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,4 +1,8 @@
-import { SaveOutlined, UploadOutlined } from "@mui/icons-material";
+import {
+  DeleteOutline,
+  SaveOutlined,
+  UploadOutlined,
+} from "@mui/icons-material";
 import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
 import { ImageGallery } from "../components";
 import { useForm } from "../../hooks/useForm";
@@ -6,6 +10,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useMemo, useRef } from "react";
 import {
   setActiveNote,
+  startDeletingNote,
   startSaveNote,
   startUploadingFiles,
 } from "../../store/journal";
@@ -50,6 +55,10 @@ export const NoteView = () => {
 
     dispatch(startUploadingFiles(files));
   };
+
+  const onDelete = () => {
+    dispatch(startDeletingNote());
+  }
 
   return (
     <Grid
@@ -117,6 +126,12 @@ export const NoteView = () => {
           value={body}
           onChange={onInputChange}
         />
+      </Grid>
+
+      <Grid container justifyContent="end">
+        <Button onClick={onDelete} sx={{ mt: 2 }} color="error">
+          <DeleteOutline />
+        </Button>
       </Grid>
 
       <ImageGallery images={note.imageUrls} />
